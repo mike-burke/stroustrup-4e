be9049ae197170e305f0ae895953451a8ff5a0af
mike-burke <mike@dub2.net>
2022-03-23 08:02:13 -0500

    Answered X3.6

---
+++ x3/06/answer.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+
+int main() {
+
+	bool b;
+	char c;
+	int i;
+	double d;
+	std::string s;
+
+	std::cout << "bool :";
+	std::cin >> b;
+
+	std::cout << "char :";
+	std::cin >> c;
+
+	std::cout << "int :";
+	std::cin >> i;
+
+	std::cout << "double :";
+	std::cin >> d;
+
+	std::cout << "string:";
+	std::cin >> s;
+
+	std::cout << "bool:" << b << "\n";
+	std::cout << "char:" << c << "\n";
+	std::cout << "int:" << i << "\n";
+	std::cout << "double:" << d << "\n";
+	std::cout << "string:" << s << "\n";
+}
