0793150da0a84a91df6fa00c051d472ea7419cf7
mike-burke <mike@dub2.net>
2022-03-23 07:57:18 -0500

    Answered X3.5

---
+++ x3/05/answer.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+int main() {
+
+	bool b = true;
+	char c = 'a';
+	int i = 42;
+	double d = 3.14;
+	std::string s = "hello";
+
+	std::cout << "bool:" << b << "\n";
+	std::cout << "char:" << c << "\n";
+	std::cout << "int:" << i << "\n";
+	std::cout << "double:" << d << "\n";
+	std::cout << "string:" << s << "\n";
+}
