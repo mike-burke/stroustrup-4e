42438e74b28f782b95d1aa0363470c31d7e0dfa5
mike-burke <mike@dub2.net>
2022-03-24 08:17:18 -0500

    Answered X5.6

--- X5/06/answer.cpp
+++ X5/06/answer.cpp
@@ -1,34 +1,53 @@
 // Redo §X.5[5], storing several (name,age) pairs in a class. Doing the reading and writing using your own >> and << operators.
 
 #import <iostream>
+#import <vector>
+
+struct Stat {
+	Stat() : age {0} {} 
+	Stat(std::string n, int a) : name{n}, age{a} {}
+
+	std::string name;
+	int age;
+};
+
+std::ostream& operator << (std::ostream& os, const Stat& s) {
+	return os << s.name << " (" << s.age << ")";
+}
+
+std::istream& operator >> (std::istream& is, Stat& s) {
+	std::string n;
+	int a;
+
+	if ((is >> n) && (n != "q") && (is >> a)) 
+		s = Stat {n,a};
+	else 
+		is.setstate(std::ios_base::failbit);
+
+	return is;
+}
+
 
 
 class Stats {
 
 	public:
-
-		Stats() : name {""}, age {0} {}
-		Stats(std::string n, int a) : name {n}, age {a} { }
-	
-		std::string get_name() const { return name; }
-		int get_age() const { return age; }
-
+		const std::vector<Stat>& get_stats() const { return stats; }
+		void add_stat(Stat s) { stats.push_back(s); }
 	private:
-		std::string name;
-		int age;
+		std::vector<Stat> stats;
 };
 
 std::ostream& operator << (std::ostream& os, const Stats& s) {
-	return os << s.get_name() << " (" << s.get_age() << ")";
+	for (auto& a : s.get_stats())
+		os << a << "\n";
+	return os;
 }
 
-std::istream& operator >> (std::istream& is, Stats& s) {
-	std::string n;
-	int a;
+std::istream& operator >> (std::istream& is, Stats& ss) {
 
-	if ((is >> n) && (is >> a)) {
-		s = Stats {n,a};
-	}
+	for (Stat s; is >> s;)
+		ss.add_stat(s);
 	return is;
 }
 
@@ -36,7 +55,8 @@ int main() {
 
 	Stats s;
 
-	std::cout << "Enter a name and an age, separated by a space: ";
+	std::cout << "Enter a set of name and an age pairs - space separated, one per line ('q' to quit) :";
+
 	std::cin >> s;
-	std::cout << s << "\n";
+	std::cout << s;
 }
