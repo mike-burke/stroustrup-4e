b3e1c67fb372756298e7cb163c7f1bc86fdb9ee6
mike-burke <mike@dub2.net>
2022-03-27 11:51:00 -0500

    Answered X7.1

---
+++ X7/01/answer.cpp
@@ -0,0 +1,7 @@
+// Get the ‘‘Hello, world!’’ program (§2.2.1) to run. This is not an exercise in programming. It is an exercise to test your use of your edit-compile-link-execute tool chain.
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, world!\n";
+}
