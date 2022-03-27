0bce153be9f0ecc8003d379cdd58c7fcbd0137c0
mike-burke <mike@dub2.net>
2022-03-27 11:49:15 -0500

    Answered X6.5

--- X6/05/answer.cpp
+++ X6/05/answer.cpp
@@ -1,5 +1,21 @@
 // Use a regex to find all decimal numbers in a file.
 
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <string>
+
+using namespace std;
+
 int main() {
 
+	ifstream ifs("numbers.txt");
+
+	regex pat (R"(\d+\.\d+)");
+	smatch matches;
+
+	for (string s;  getline(ifs,s); ) {
+		if (regex_search(s,matches,pat))
+			cout << matches[0] << "\n";
+	}
 }
